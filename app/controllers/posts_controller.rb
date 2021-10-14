7baa8d278e939d5903b395aebffe50438e6c16a2
YukiKakegawa <yuki1993525@gmail.com>
2021-10-14 13:01:54 +0000

    [Create]新規投稿

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+  def new
+    @posts= Post.new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
