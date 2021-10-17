05c7d2a591d1f02feb4a9e68304d167afbbed4d2
YukiKakegawa <yuki1993525@gmail.com>
2021-10-17 12:19:44 +0000

    [Create]いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+
+  def gooded_by?(user)
+    goods.where(user_id: user_id).exists?
+  end
+
 end
